6679c1c3de5bf792dce379f7e3d4059e585970a9
piekie <binnonnorie@gmail.com>
2016-05-14 14:21:59 +0300

    Initial 1.1

--- app/src/main/java/com/dna/cactusoverwatch/TestActivity.java
+++ app/src/main/java/com/dna/cactusoverwatch/TestActivity.java
@@ -3,11 +3,20 @@ package com.dna.cactusoverwatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        Firebase.setAndroidContext(this);
+        Firebase root = new Firebase("https://amanda.firebaseio.com/");
+
+        String s = "hello world";
+
+        root.setValue(s);
     }
 }
