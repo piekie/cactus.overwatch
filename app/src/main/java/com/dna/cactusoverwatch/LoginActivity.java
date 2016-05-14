1957a0fb42101d1fc919cc176a6d7c32e0eed2de
Alex <kll.a.s@yandex.ru>
2016-05-14 15:47:58 +0300

    login test

---
+++ app/src/main/java/com/dna/cactusoverwatch/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.dna.cactusoverwatch;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnLogin = (Button)findViewById(R.id.button_login);
+        Button btnSignUp = (Button)findViewById(R.id.button_sign_up);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
