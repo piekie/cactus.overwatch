b89ee30ad8780ac1927dca54ab0afc1a1b0c616a
piekie <binnonnorie@gmail.com>
2016-05-15 03:28:06 +0300

    trying to refresh

--- app/src/main/java/com/dna/cactusoverwatch/MainActivity.java
+++ app/src/main/java/com/dna/cactusoverwatch/MainActivity.java
@@ -1,19 +1,16 @@
 package com.dna.cactusoverwatch;
 
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,24 +18,6 @@ import com.astuetz.PagerSlidingTabStrip;
 import com.dna.cactusoverwatch.fragments.FragmentListActual;
 import com.dna.cactusoverwatch.fragments.FragmentListConflict;
 import com.dna.cactusoverwatch.utils.ApiGetter;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.shaded.apache.http.HttpEntity;
-import org.shaded.apache.http.HttpResponse;
-import org.shaded.apache.http.HttpStatus;
-import org.shaded.apache.http.StatusLine;
-import org.shaded.apache.http.client.ClientProtocolException;
-import org.shaded.apache.http.client.HttpClient;
-import org.shaded.apache.http.client.methods.HttpGet;
-import org.shaded.apache.http.impl.client.DefaultHttpClient;
-import org.shaded.apache.http.util.EntityUtils;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -47,11 +26,6 @@ public class MainActivity extends AppCompatActivity
     private ViewPager pager;
     private MyPagerAdapter adapter;
     private Toolbar toolbar;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,9 +78,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
         ApiGetter ap = new ApiGetter();
         ap.getTenders(0);
@@ -120,45 +91,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.dna.cactusoverwatch/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.dna.cactusoverwatch/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 
     public class MyPagerAdapter extends FragmentPagerAdapter {
 
--- app/src/main/java/com/dna/cactusoverwatch/utils/TenderCutted.java
+++
@@ -1,7 +0,0 @@
-package com.dna.cactusoverwatch.utils;
-
-/**
- * Created by piekie on 5/15/2016.
- */
-public class TenderCutted {
-}
